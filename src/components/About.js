4b019ded8ea881a3b3d6254a40ae2a5cf4604de1
Frédérick Van Isschot <46893914+Fred-Vaniss@users.noreply.github.com>
2019-09-25 15:33:02 +0200

    Validation formulaire de contact

--- src/components/About.js
+++ src/components/About.js
@@ -6,9 +6,11 @@ export default class About extends Component {
 			<section id="about">
 				<div className="wrapper">
 					<h2>À propos de moi</h2>
-					<p>J’ai commencé ma carrière en tant qu’infographiste où j’ai touché un peu tous les différents outils. J’ai ensuite décidé de me spécialiser dans le développement web où j’ai commencé dans l’UX Webdesign.</p>
-					<p>J’ai développé une vraie passion pour le code, j’adore découvrir de nouvelles choses et les expérimenter et j’ai un grand esprit d’analyse et de logique.</p>
-					<p>Je suis aussi compétent en front-end qu’en back-end mais j’ai une préférence pour le front-end.</p>
+					<p>J’ai commencé ma carrière en tant qu’infographiste où j’ai touché un peu tous les différents outils: retouche photo, web, animation, sonorisation, etc. </p>
+
+					<p>N'ayant que la base de tout, il m'a fallu faire un choix: où me spécialiser? J'ai donc décidé de suivre la voie du développement web ou j'ai d'abord suivi des cours en ligne en autodidacte avant de suivre des formations dans le web.</p>
+
+					<p>Depuis, j'ai développé une vraie passion pour la programmation, j'adore chipoter un peu partout, trouver des solutions et résoudre des problèmes.</p>
 				</div>
 			</section>
 		)
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,7 +11,12 @@ export default class Contact extends Component {
 				phone:"",
 				msg:"",
 			},
-			validation: {},
+			validation: {
+				name: false,
+				mail: false,
+				phone: false,
+				msg: false
+			},
 			labelClass: {
 				name:"",
 				mail:"",
@@ -34,35 +39,68 @@ export default class Contact extends Component {
 		if (field === "name"){
 			validation[field] = (e.target.value.length > 1) ? true : false;
 		} else if (field === "mail"){
-			let regex = ""
-			validation[field] = regex.test(e.target.value)
+			const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+			validation[field] = regex.test(e.target.value);
+		} else if (field === "phone"){
+			// eslint-disable-next-line
+			const regex = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/;
+			validation[field] = regex.test(e.target.value);
+		} else {
+			validation[field] = (e.target.value.length > 10) ? true : false;
 		}
+
+		console.table(validation);
 	}
 
+	validateForm(e){
+		
 
+		let allValid = true;
+		
+		const forms = {
+			name: this.refs.name,
+			mail: this.refs.mail,
+			phone: this.refs.phone,
+			msg: this.refs.msg
+		}
+
+		for (const key in forms) {
+			const inputName = forms[key].id
+
+			if (this.state.validation[inputName] === true){
+				forms[inputName].classList.remove("field-error")
+			} else {
+				forms[inputName].classList.add("field-error")
+				allValid = false;
+			}
+		}
+
+		if (!allValid) {e.preventDefault();}
+	}
 
 	render() {
 		return (
 			<section id="contact">
 				<div className="wrapper">
 					<h2>Contactez-moi</h2>
-					<form action="#" method="post">
+					<form action="/mailer.php" method="post">
 						<div className="form-group">
-							<input type="text" name="name" id="name" onChange={this.handleChanges.bind(this, "name")} value={this.state.fields["name"]} required />
+							<input type="text" name="name" id="name" onChange={this.handleChanges.bind(this, "name")} value={this.state.fields["name"]} ref="name" required />
 							<label htmlFor="name" className={this.state.labelClass.name}>Nom et prénom</label><br/>
 						</div>
 						<div className="form-group">
-							<input type="email" name="mail" id="mail" onChange={this.handleChanges.bind(this, "mail")} value={this.state.fields["mail"]} required/>
+							<input type="email" name="mail" id="mail" onChange={this.handleChanges.bind(this, "mail")} value={this.state.fields["mail"]} ref="mail" required/>
 							<label htmlFor="mail" className={this.state.labelClass.mail}>Adresse e-mail</label><br/>
 						</div>
 						<div className="form-group">
-							<input type="text" name="phone" id="phone" onChange={this.handleChanges.bind(this, "phone")} value={this.state.fields["phone"]} required/>
+							<input type="text" name="phone" id="phone" onChange={this.handleChanges.bind(this, "phone")} value={this.state.fields["phone"]} ref="phone" required/>
 							<label htmlFor="phone" className={this.state.labelClass.phone}>Téléphone</label><br/>
 						</div>
 						<div className="form-group" required>
-							<textarea name="msg" id="msg" onChange={this.handleChanges.bind(this, "msg")} value={this.state.fields["msg"]} required></textarea>
+							<textarea name="msg" id="msg" onChange={this.handleChanges.bind(this, "msg")} value={this.state.fields["msg"]} ref="msg" required></textarea>
 							<label htmlFor="msg" className={this.state.labelClass.msg}>Message</label><br/>
 						</div>
+						<button className="form-submit" onClick={this.validateForm.bind(this)}>Envoyer</button>
 					</form>
 				</div>
 			</section>
