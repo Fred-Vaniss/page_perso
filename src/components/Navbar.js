b306e99578441fc24118dff7cc735b6738e0bacd
Frédérick Van Isschot <46893914+Fred-Vaniss@users.noreply.github.com>
2019-10-01 12:49:39 +0200

    Correction menu responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -27,23 +27,32 @@ export default class Navbar extends Component {
 		console.log(this.state.menuOpen)
 	}
 
+	closeMenu = () => {
+		if (this.state.menuOpen === true){
+			this.setState({menuOpen: false});
+		}
+	}
+
 	render(){
 		return(
-			<div className={(this.state.sticky) ? "nav-bar sticky" : "nav-bar"} ref="navBar">
-				<button onClick={() => this.openMenu()} className="burger-button">Menu</button>
-				<div className={`wrapper menu ${(this.state.menuOpen) ? "menu-open" : ""}`}>
-					<a href={Cv} className="download-cv">Télécharger le CV</a>
-					<nav>
-						<ul>
-							<li><a href="#about">À propos</a></li>
-							<li><a href="#skills">Compétences</a></li>
-							<li><a href="#experience">Parcours</a></li>
-							<li><a href="#portfolio">Portfolio</a></li>
-							<li><a href="#contact">Contact</a></li>
-						</ul>
-					</nav>
+			<>
+				<div className={`menu-dim ${this.state.menuOpen ? "dimmer" : ""}`} onClick={() => this.closeMenu()}></div>
+				<div className={(this.state.sticky) ? "nav-bar sticky" : "nav-bar"} ref="navBar">
+					<button onClick={() => this.openMenu()} className="burger-button">Menu</button>
+					<div className={`wrapper menu ${(this.state.menuOpen) ? "menu-open" : ""}`}>
+						<a href={Cv} className="download-cv">Télécharger le CV</a>
+						<nav>
+							<ul>
+								<li><a href="#about" onClick={() => this.closeMenu()}>À propos</a></li>
+								<li><a href="#skills" onClick={() => this.closeMenu()}>Compétences</a></li>
+								<li><a href="#experience" onClick={() => this.closeMenu()}>Parcours</a></li>
+								<li><a href="#portfolio" onClick={() => this.closeMenu()}>Portfolio</a></li>
+								<li><a href="#contact" onClick={() => this.closeMenu()}>Contact</a></li>
+							</ul>
+						</nav>
+					</div>
 				</div>
-			</div>
+			</>
 		)
 	}
 }
