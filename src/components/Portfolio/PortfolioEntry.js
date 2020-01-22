d9aff205f056b810f7d2bcfe94aadef8ca0a0faf
Frédérick Van Isschot <46893914+Fred-Vaniss@users.noreply.github.com>
2020-01-22 17:19:12 +0100

    Style du modal

--- src/components/Portfolio/PortfolioEntry.js
+++ src/components/Portfolio/PortfolioEntry.js
@@ -4,6 +4,25 @@ import { faGlobe, faStickyNote, faTimes } from "@fortawesome/free-solid-svg-icon
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 export default class PortfolioEntry extends Component {
+	constructor(){
+		super()
+
+		this.state={
+			class: ""
+		}
+	}
+	
+	toggleModal(action){
+		console.log('action: '+action)
+		if (action){
+			this.setState({class: "shown"})
+			document.body.style = "overflow-y: hidden"
+		} else {
+			this.setState({class: ""})
+			document.body.style = ""
+		}
+	}
+
 	render() {
 		const { item, number } = this.props
 		const img = require(`./img/${item.img}.jpg`)
@@ -17,7 +36,7 @@ export default class PortfolioEntry extends Component {
 							<h4>{item.title}</h4>
 							<p>{item.techno}</p>
 							<div className="portfolio-links">
-								{item.url.note && <button className="portfolio-link" title="Plus de détails" data-port-id={number}><span><FontAwesomeIcon icon={faStickyNote}/></span></button>}
+								{item.url.note && <button className="portfolio-link" title="Plus de détails" onClick={() => this.toggleModal(true)} data-port-id={number}><span><FontAwesomeIcon icon={faStickyNote}/></span></button>}
 								{item.url.git && <a className="portfolio-link" href={item.url.git} target="_blank" rel="noopener noreferrer" title="Dépot Github"><FontAwesomeIcon icon={faGithub}/></a>}
 								{item.url.preview && <a className="portfolio-link" href={item.url.preview} target="_blank" rel="noopener noreferrer" title="Page du projet"><FontAwesomeIcon icon={faGlobe}/></a>}
 							</div>
@@ -26,10 +45,11 @@ export default class PortfolioEntry extends Component {
 					<img src={img} alt={item.title}/>
 				</div>
 				{item.url.note && 
-					<div id={"modal-port-"+number} className="portfolio-modal">
+					<div id={"modal-port-"+number} className={"portfolio-modal "+this.state.class}>
 						<div className="modal-box">
-							<button className="modal-close"><FontAwesomeIcon icon={faTimes}/></button>
+							<button className="modal-close"><FontAwesomeIcon icon={faTimes} onClick={() => this.toggleModal(false)}/></button>
 							{item.url.note}
+							<button className="form-submit modal-close-bottom" onClick={() => this.toggleModal(false)}>Fermer</button>
 						</div>
 					</div>
 
