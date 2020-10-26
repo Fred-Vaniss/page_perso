e9d09b78c858b48b0cf8a71598161b8968bdfe09
Frédérick Van Isschot <46893914+Fred-Vaniss@users.noreply.github.com>
2020-10-26 13:28:13 +0100

    Langue définie via l'URL

--- src/App.js
+++ src/App.js
@@ -1,13 +1,56 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
+import { BrowserRouter as Router, Route, useLocation } from 'react-router-dom'
 import { Intro, About, Timeline, Skills, Portfolio, Contact, Footer} from './components';
 
 const App = props => {
+  return (
+    <Router>
+      <Route path="/">
+        <QueryApp/>
+      </Route>
+    </Router>
+  );
+} 
+export default App;
+
+const useQuery = () => {
+  return new URLSearchParams(useLocation().search);
+}
+
+const QueryApp = () => {
 
   const [lang, setLang] = useState('fr')
 
+  const setQueryString = (lang) => {
+    const newUrl = window.location.protocol + "//" +
+                  window.location.host +
+                  window.location.pathname +
+                  '?lang='+lang;
+
+    window.history.pushState({ path: newUrl }, "", newUrl)
+  }
+
+  let query = useQuery()
+
+  const firstLoad = useRef(true)
+
+  if (firstLoad.current) {
+    if (!query.get('lang')) {
+      setQueryString(lang)
+    } else {
+      setLang(query.get('lang'))
+    }
+    firstLoad.current = false 
+  }
+
   const handleLangButton = e => {
     lang === 'fr' ? setLang('en') : setLang('fr');
+    setQueryString(lang)
   }
+
+  useEffect(() => {
+    setQueryString(lang)
+  }, [lang])
 
   return (
     <>
@@ -19,7 +62,5 @@ const App = props => {
       <Contact lang={lang}/>
       <Footer/>
     </>
-  );
-} 
-
-export default App;
+  )
+}
